84327afe275bad3997681b96bc4e2a836df6b974
Oguri Cap <130316718+Just-Oguri-Cap@users.noreply.github.com>
2026-03-06 15:41:35 -0300

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.adapter_fases_lunar {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.adapter_fases_lunar to javafx.fxml;
-    exports org.example.adapter_fases_lunar;
-}
--- src/main/java/org/example/adapter_fases_lunar/FonteLunarCientifica.java
+++
@@ -1,8 +0,0 @@
-package org.example.adapter_fases_lunar;
-
-import java.time.LocalDate;
-
-public interface FonteLunarCientifica {
-    int getDiaDoCiclo(LocalDate data);
-    double getPercentualIluminacao(LocalDate data);
-}
